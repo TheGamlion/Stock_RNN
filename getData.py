08864ae61ccaba6787cd2a4ecf0d846046a3529b
stefan <thegamlion@gmail.com>
2019-08-01 02:19:27 +0200

    get data funktioniert

--- getData.py
+++ getData.py
@@ -2,22 +2,25 @@ import os
 import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 import csv
+import matplotlib.pyplot as plt
 
 API_KEY = 'CX5IL832J9PVYSA0'
+plt.close('all')
 
 ts = TimeSeries(key= API_KEY,output_format= 'pandas') 
 
-#fetch data
-# 1 file load needed stocks -> save into list
-# export the data as name.csv
-
+stocklist = []
 with open('stock_Name.csv','r') as f:
     reader = csv.reader(f)
     for name in reader:
-        print(name)
+        stocklist = name
 
+stock=[] 
+for x in name:
+    data,meta_data = ts.get_daily(symbol=x,outputsize='full')
+    data.to_csv('data/'+x+'.csv',encoding='utf-8', index=False)    
+    stock.append(data)
 
-
-
-#data,meta_data = ts.get_daily(symbol = 'NVDA',outputsize='compact')
-
+for s in stock:
+     s['1. open'].plot()
+plt.show()
